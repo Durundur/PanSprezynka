e8cf90a9e79f46f2c9979d897f66e780f59795f0
Durundur <0bogusz0@gmail.com>
2022-09-23 21:57:17 +0200

    refactor to functional components

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,40 +1,43 @@
-import React from "react";
-import searchIcon from './icons8-search.svg'
-import './TopBar.css'
+import React, { useState } from "react";
+import searchIcon from "./icons8-search.svg";
+import "./TopBar.css";
 
-class TopBar extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { searchInputVal: '' }
-        this.handleInputChange = this.handleInputChange.bind(this)
-        this.submitSearch = this.submitSearch.bind(this)
-        this.handelEnter = this.handelEnter.bind(this)
-    }
-    handleInputChange(event) {
-        this.setState({ searchInputVal: event.target.value })
-    }
-    submitSearch() {
-        this.props.onSearchSubmit(this.state.searchInputVal)
-        this.setState({ searchInputVal: '' })
-    }
-    handelEnter(e) {
-        if (e.key === 'Enter') this.submitSearch()
-    }
-    // handleSortChange(event) {
-    //     this.setState({ sortOrder: event.target.value })
-    // }
-    render() {
-        return (
-            <div className="top-bar">
-                <span className='top-bar__input'>
-                    <input className='search-input' onKeyDown={this.handelEnter} type={'text'} list={'streamers'} placeholder='Wyszukaj' value={this.state.searchInputVal} onChange={this.handleInputChange}></input>
-                    <img className='search-icon' alt='wyszukaj' onClick={this.submitSearch} src={searchIcon}></img>
-                </span>
-                <span className='top-bar__sort'>
-                </span>
-            </div>
-        )
-    }
+function TopBar(props) {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+  const submitSearch = () => {
+    props.onSearchSubmit(searchInput);
+    setSearchInput("");
+  };
+  const handelEnter = (e) => {
+    console.log(props)
+    if (e.key === "Enter") submitSearch();
+  };
+  return (
+    <div className="top-bar">
+      <span className="top-bar__input">
+        <input
+          className="search-input"
+          onKeyDown={handelEnter}
+          type={"text"}
+          list={"streamers"}
+          placeholder="Wyszukaj"
+          value={searchInput}
+          onChange={handleInputChange}
+        ></input>
+        <img
+          className="search-icon"
+          alt="wyszukaj"
+          onClick={submitSearch}
+          src={searchIcon}
+        ></img>
+      </span>
+      <span className="top-bar__sort"></span>
+    </div>
+  );
 }
 
-export default TopBar
+export default TopBar;
