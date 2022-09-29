ee4983cfd5a56b5167d4b7e4fd232f5f124bc015
Durundur <0bogusz0@gmail.com>
2022-09-29 12:03:37 +0200

    v2.02

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TopBar from './TopBar';
 import ChartsList from './ChartsList';
 import NotFound from './NotFound';
-import About from './About'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/lista' element={[<TopBar/>,<About/>]}/>
-        <Route path='/' element={[<TopBar/>,<ChartsList/>]}/>
-        <Route path='/:channelName' element={[<TopBar/>,<ChartsList/>]}/>
-        <Route path='*' element={[<TopBar/>,<NotFound/>]}/>
+      <Route path='/' element={[<TopBar/>,<ChartsList/>]}>
+        <Route path=':channelName' element={[<TopBar/>,<ChartsList/>]}>
+          <Route path='*' element={[<TopBar/>,<NotFound/>]}></Route>
+        </Route>
+      </Route>
       </Routes>
     </BrowserRouter>
   )
